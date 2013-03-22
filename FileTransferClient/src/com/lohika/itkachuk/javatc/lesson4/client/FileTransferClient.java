02fbfd49e20e2def83fbc92207d75723a54cefe6
itkachuk <itkachuk.btm@gmail.com>
2013-03-22 10:17:46 +0200

    Added server/client modules

---
+++ FileTransferClient/src/com/lohika/itkachuk/javatc/lesson4/client/FileTransferClient.java
@@ -0,0 +1,111 @@
+package com.lohika.itkachuk.javatc.lesson4.client;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Arrays;
+
+import com.lohika.itkachuk.javatc.lesson4.server.FileTransferServer;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: itkachuk
+ * Date: 3/18/13 3:30 PM
+ */
+public class FileTransferClient {
+
+    private String host;
+    private int port;
+    private String filePath;
+    private String targetPath;
+
+    public FileTransferClient(String host, int port, String filePath, String targetPath) {
+        this.host = host;
+        this.port = port;
+        this.filePath = filePath;
+        this.targetPath = targetPath;
+    }
+
+    private void validateInputs() throws IllegalArgumentException {
+        if (StringUtils.isEmpty(host)) {
+            throw new IllegalArgumentException("Input error: server host name can not be empty");
+        }
+        if (port < 1 || port > 65535) {
+            throw new IllegalArgumentException("Input error: server port should be from range 1-65535");
+        }
+
+        if (StringUtils.isEmpty(filePath)) {
+            throw new IllegalArgumentException("Input error: file path can not be empty");
+        }
+        if (filePath.length() > 260) {
+            throw new IllegalArgumentException("Input error: the total file path length can not be more than 260 characters");
+        }
+
+        File fileToSend = new File(filePath);
+        if (!fileToSend.isFile()) {
+            throw new IllegalArgumentException("Input error: \"" + filePath + "\" is not a file");
+        }
+        if (StringUtils.isNotEmpty(targetPath)) {
+            if ((targetPath.length() + fileToSend.getName().length()) > 260) {
+                throw new IllegalArgumentException("Input error: the total path length of target file can not be more than 260 characters");
+            }
+        }
+    }
+
+    public void uploadFile() throws IOException {
+        File file = new File(filePath);
+        ObjectInputStream ois = null;
+        ObjectOutputStream oos = null;
+        try {
+            Socket socket = new Socket(host, port);
+            ois = new ObjectInputStream(socket.getInputStream());
+            oos = new ObjectOutputStream(socket.getOutputStream());
+
+            oos.writeObject(file.getName());
+            if (StringUtils.isNotEmpty(targetPath)) {
+                oos.writeObject(targetPath);
+            } else {
+                oos.writeObject(FileTransferServer.DEFAULT_TARGET_PATH);
+            }
+
+            FileInputStream fis = new FileInputStream(file);
+            byte [] buffer = new byte[FileTransferServer.BUFFER_SIZE];
+            Integer bytesRead;
+
+            while ((bytesRead = fis.read(buffer)) > 0) {
+                oos.writeObject(bytesRead);
+                oos.writeObject(Arrays.copyOf(buffer, buffer.length));
+            }
+        } finally {
+            try {
+                oos.close();
+                ois.close();
+            } catch (Exception e) {}
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        if (args.length != 3 && args.length != 4) {
+            System.out.println("Usage: java FileTransferClient <host>:<port> <file path> [<target path>]");
+            System.exit(1);
+        }
+
+        FileTransferClient fileTransferClient;
+        try {
+            if (args.length == 3)
+                fileTransferClient = new FileTransferClient(args[0], Integer.valueOf(args[1]), args[2], null);
+            else
+                fileTransferClient = new FileTransferClient(args[0], Integer.valueOf(args[1]), args[2], args[3]);
+
+            fileTransferClient.validateInputs();
+
+            fileTransferClient.uploadFile();
+
+        } catch (NumberFormatException nfe) {
+            System.out.println("One or more input arguments are not valid:\n\t port is not a valid integer number");
+        } catch (IllegalArgumentException iae) {
+            System.out.println("One or more input arguments are not valid:\n\t" + iae.getMessage());
+        }
+    }
+}
---
+++ FileTransferServer/src/com/lohika/itkachuk/javatc/lesson4/server/FileTransferServer.java
@@ -0,0 +1,128 @@
+package com.lohika.itkachuk.javatc.lesson4.server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: itkachuk
+ * Date: 3/18/13 3:21 PM
+ */
+
+public class FileTransferServer extends Thread {
+    private int port;
+    public static final int BUFFER_SIZE = 1024;
+    public static final String DEFAULT_TARGET_PATH = "use current directory";
+
+    @Override
+    public void run() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(getPort());
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                saveFile(socket);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        if (port < 1 || port > 65535) {
+            throw new IllegalArgumentException("Input error: server port should be from range 1-65535");
+        }
+        this.port = port;
+    }
+
+    private void saveFile(Socket socket) throws Exception {
+        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+        FileOutputStream fos = null;
+        byte [] buffer;
+        String fileName = null;
+        String targetPath = null;
+
+        // 1. Read file name.
+        Object o = ois.readObject();
+        if (o instanceof String) {
+            fileName = o.toString();
+        } else {
+            throwException("File transfer error: file name wasn't received");
+        }
+
+        // 2. Read target path
+        o = ois.readObject();
+        if (o instanceof String) {
+            targetPath = o.toString();
+        } else {
+            throwException("File transfer error: file target path wasn't received");
+        }
+
+        // build file name and create FileOutputStream
+        if (targetPath.equals(DEFAULT_TARGET_PATH)) {
+            fos = new FileOutputStream(fileName); // use current directory for saving file
+        } else {
+            File targetDir = new File(targetPath);
+            if (!targetDir.isDirectory()) {
+                throwException("File transfer error: wrong target path (not a directory)");
+            } else {
+                fos = new FileOutputStream(targetDir + File.separator + fileName); // use received target path
+            }
+        }
+
+        // 3. Read file to the end.
+        Integer bytesRead;
+        do {
+            o = ois.readObject();
+
+            if (!(o instanceof Integer)) {
+                throwException("File transfer error: buffer size wasn't received");
+            }
+
+            bytesRead = (Integer)o;
+
+            o = ois.readObject();
+
+            if (!(o instanceof byte[])) {
+                throwException("File transfer error: bytes array reading failed");
+            }
+
+            buffer = (byte[])o;
+
+            // 4. Write data to output file.
+            fos.write(buffer, 0, bytesRead);
+        } while (bytesRead == BUFFER_SIZE);
+
+        fos.close();
+        ois.close();
+        oos.close();
+    }
+
+    public static void throwException(String message) throws Exception {
+        throw new Exception(message);
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java FileTransferServer <port>");
+            System.exit(1);
+        }
+        int port = 0;
+        try {
+            port = Integer.valueOf(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("Input error: port parameter is not a number");
+            System.exit(1);
+        }
+
+        FileTransferServer server = new FileTransferServer();
+        server.setPort(port);
+        server.start();
+    }
+}
